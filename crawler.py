44529df02742fc7e2d48c139573df5052436b871
skisssbb <skisssbb@hotmail.de>
2017-05-01 10:18:43 +0200

    first line

--- crawler.py
+++ crawler.py
@@ -1,1 +1,6 @@
-#this is a comment
+import requests
+import re
+import urlparse
+
+re = requests.get("http://www.spiegel.de/politik/ausland/venezuela-vier-szenarien-fuer-die-zukunft-des-krisenlandes-a-1145434.html")
+print re.
